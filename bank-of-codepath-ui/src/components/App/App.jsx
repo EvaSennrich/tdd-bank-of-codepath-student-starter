97aa94e725d3c9b539a9a03344f986ed27c1c3b0
Eva Sennrich <evasennrich@gmail.com>
2022-06-28 14:49:40 -0700

    Routes set up

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,24 @@
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Home from "../Home/Home";
+import "./App.css";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import TransactionDetail from "../TransactionDetail/TransactionDetail";
+// import TransactionalDetail from "../TransactionalDetail/TransactionalDetail";
 
 export default function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/transactions/:trasactionId" element={<TransactionDetail />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
